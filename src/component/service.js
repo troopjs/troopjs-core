1a19aa870e05a76155d4dcfc211f6eb8490ee4fe
Mikael Karon <mikael@karon.se>
2012-02-20 13:53:40 +0800

    added service component

---
+++ src/component/service.js
@@ -0,0 +1,10 @@
+/*!
+ * TroopJS service component
+ * @license TroopJS 0.0.1 Copyright 2012, Mikael Karon <mikael@karon.se>
+ * Released under the MIT license.
+ */
+define([ "./base" ], function ServiceComponentModule(Component) {
+	return Component.extend({
+		displayName : "component/service"
+	});
+});
