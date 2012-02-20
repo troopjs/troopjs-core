285d8bea8e3ff1d9b6d67860d77b6a452f249835
Mikael Karon <mikael@karon.se>
2012-02-20 14:31:45 +0800

    moved all 'base' behavior from base to gadget, only keeping instance counting and config on the base

--- src/component/base.js
+++ src/component/base.js
@@ -3,49 +3,14 @@
  * @license TroopJS 0.0.1 Copyright 2012, Mikael Karon <mikael@karon.se>
  * Released under the MIT license.
  */
-define([ "compose", "config", "pubsub/hub", "pubsub/topic", "deferred" ], function BaseComponentModule(Compose, config, hub, Topic, Deferred) {
-	var PUBLISH = hub.publish;
-	var SUBSCRIBE = hub.subscribe;
-	var UNSUBSCRIBE = hub.unsubscribe;
+define([ "compose", "config" ], function BaseComponentModule(Compose, config) {
 	var COUNT = 0;
 
-	return Compose(function BaseComponent() {
+	return Compose(function Component() {
 		this.instanceCount = COUNT++;
 	}, {
 		config : config,
 		displayName : "component/base",
-
-		publish : function publish() {
-			var self = this;
-
-			PUBLISH.apply(hub, arguments);
-
-			return self;
-		},
-
-		subscribe : function subscribe() {
-			var self = this;
-
-			SUBSCRIBE.apply(hub, arguments);
-
-			return self;
-		},
-
-		unsubscribe : function unsubscribe() {
-			var self = this;
-
-			UNSUBSCRIBE.apply(hub, arguments);
-
-			return self;
-		},
-
-		request : function request(setting, deferred) {
-			var self = this;
-
-			self.publish(new Topic("app/request", self), setting, deferred);
-
-			return self;
-		},
 
 		toString : function toString() {
 			var self = this;
--- src/component/gadget.js
+++ src/component/gadget.js
@@ -3,8 +3,44 @@
  * @license TroopJS 0.0.1 Copyright 2012, Mikael Karon <mikael@karon.se>
  * Released under the MIT license.
  */
-define([ "./base" ], function GadgetComponentModule(Component) {
+define([ "./base", "pubsub/hub", "pubsub/topic", "deferred" ], function GadgetComponentModule(Component, hub, Topic, Deferred) {
+	var PUBLISH = hub.publish;
+	var SUBSCRIBE = hub.subscribe;
+	var UNSUBSCRIBE = hub.unsubscribe;
+
 	return Component.extend({
-		displayName : "component/gadget"
+		displayName : "component/gadget",
+
+		publish : function publish() {
+			var self = this;
+
+			PUBLISH.apply(hub, arguments);
+
+			return self;
+		},
+
+		subscribe : function subscribe() {
+			var self = this;
+
+			SUBSCRIBE.apply(hub, arguments);
+
+			return self;
+		},
+
+		unsubscribe : function unsubscribe() {
+			var self = this;
+
+			UNSUBSCRIBE.apply(hub, arguments);
+
+			return self;
+		},
+
+		request : function request(setting, deferred) {
+			var self = this;
+
+			self.publish(new Topic("app/request", self), setting, deferred);
+
+			return self;
+		},
 	});
 });
