979c499280e7c876d94f09d38e0a0a5cf6b4a3dc
Mikael Karon <mikael@karon.se>
2012-02-20 14:28:30 +0800

    refactored service to gadget.

--- src/component/gadget.js
+++ src/component/gadget.js
@@ -1,10 +1,10 @@
 /*!
- * TroopJS service component
+ * TroopJS gadget component
  * @license TroopJS 0.0.1 Copyright 2012, Mikael Karon <mikael@karon.se>
  * Released under the MIT license.
  */
-define([ "./base" ], function ServiceComponentModule(Component) {
+define([ "./base" ], function GadgetComponentModule(Component) {
 	return Component.extend({
-		displayName : "component/service"
+		displayName : "component/gadget"
 	});
 });
