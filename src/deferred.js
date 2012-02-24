ef4d5d462a821bcbab6f593fa82a0840c62db895
Mikael Karon <mikael@karon.se>
2012-02-24 17:56:53 +0800

    asume 'deferred' module is provided as core

--- src/deferred.js
+++
@@ -1,8 +0,0 @@
-/*!
- * TroopJS deferred component
- * @license TroopJS 0.0.1 Copyright 2012, Mikael Karon <mikael@karon.se>
- * Released under the MIT license.
- */
-define([ "jquery" ], function DeferredModule($) {
-	return $.Deferred;
-});
