0ee8b87f773a4c577fa3c30d5562f4c64a6365f5
Mikael Karon <mikael@karon.se>
2012-03-12 14:47:08 +0800

    we don't want the store to expose over hub

--- src/store/base.js
+++ src/store/base.js
@@ -4,25 +4,7 @@
  * Released under the MIT license.
  */
 define([ "compose", "../component/gadget" ], function StoreModule(Compose, Gadget) {
-	var SLICE = Array.prototype.slice;
-
-	function StoreProxy(target) {
-		return function storeProxy() {
-			return target.apply(this, SLICE.call(arguments, 1));
-		};
-	}
-
-	return Gadget.extend(function Store() {
-		var self = this;
-
-		var displayName = self.displayName;
-
-		self
-			.subscribe("hub/" + displayName + "/get", self, StoreProxy(self.get))
-			.subscribe("hub/" + displayName + "/set", self, StoreProxy(self.set))
-			.subscribe("hub/" + displayName + "/remove", self, StoreProxy(self.remove))
-			.subscribe("hub/" + displayName + "/clear", self, StoreProxy(self.clear));
-	}, {
+	return Gadget.extend({
 		set : Compose.required,
 		get : Compose.required,
 		remove : Compose.required,
