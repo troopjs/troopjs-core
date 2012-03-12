b40e098975fa15225b2158962788c03d0b65ae8c
Mikael Karon <mikael@karon.se>
2012-03-12 17:39:29 +0800

    add (unshimed) sessionStorage.
    only resolve deferred if possible

--- src/store/session.js
+++ src/store/session.js
@@ -3,27 +3,52 @@
  * @license TroopJS 0.0.1 Copyright 2012, Mikael Karon <mikael@karon.se>
  * Released under the MIT license.
  */
-define([ "compose", "./base" ], function StoreSessionModule(Compose, Store) {
+define([ "compose", "./base", "json" ], function StoreSessionModule(Compose, Store, json) {
 
-	return Compose.create(Store, function StoreSession() {
-		this.clear();
-	}, {
+	// Grab session storage
+	var STORAGE = window.sessionStorage;
+
+	return Compose.create(Store, {
 		displayName : "store/session",
 
 		set : function set(key, value, deferred) {
-			deferred.resolve(this.storage[key] = value);
+			// JSON encoded 'value' then store as 'key'
+			STORAGE.setItem(key, json.stringify(value));
+
+			// Resolve deferred
+			if (deferred && deferred.resolve instanceof Function) {
+				deferred.resolve(value);
+			}
 		},
 
 		get : function get(key, deferred) {
-			deferred.resolve(this.storage[key]);
+			// Get value from 'key', parse JSON
+			var value = json.parse(STORAGE.getItem(key));
+
+			// Resolve deferred
+			if (deferred && deferred.resolve instanceof Function) {
+				deferred.resolve(value);
+			}
 		},
 
 		remove : function remove(key, deferred) {
-			deferred.resolve(delete this.storage[key]);
+			// Remove key
+			STORAGE.removeItem(key);
+
+			// Resolve deferred
+			if (deferred && deferred.resolve instanceof Function) {
+				deferred.resolve();
+			}
 		},
 
-		clear : function clear() {
-			this.storage = {};
+		clear : function clear(deferred) {
+			// Clear
+			STORAGE.clear();
+
+			// Resolve deferred
+			if (deferred && deferred.resolve instanceof Function) {
+				deferred.resolve();
+			}
 		}
 	});
 });
