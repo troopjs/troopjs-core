39f60727b3c6c1e2ce2685a0d660fc74283fc346
Mikael Karon <mikael@karon.se>
2012-04-15 16:10:07 +0800

    added initial implementations of route/router and route/placeholder

---
+++ src/route/router.js
@@ -0,0 +1,28 @@
+/*!
+ * TroopJS route/router module
+ * @license TroopJS 0.0.1 Copyright 2012, Mikael Karon <mikael@karon.se>
+ * Released under the MIT license.
+ */
+define([ "../component/widget", "../util/uri", "callbacks" ], function RouterModule(Widget, URI, Callbacks) {
+	var CALLBACKS = "callbacks";
+
+	return Widget.extend(function Router($element, name) {
+		this[CALLBACKS] = Callbacks("memory");
+	}, {
+		"dom/hashchange" : function onHashChange(topic, $event) {
+			this.publish("route", URI($event.target.location.hash.replace(/^#/, "")));
+		},
+
+		"hub/route" : function fireRoute(topic, uri) {
+			this[CALLBACKS].fire(uri);
+		},
+
+		"hub/route:add" : function addRoute(topic, callback) {
+			this[CALLBACKS].add(callback);
+		},
+
+		"hub/route:remove" : function removeRoute(topic, callback) {
+			this[CALLBACKS].remove(callback);
+		}
+	});
+});
