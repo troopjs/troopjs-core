8ffb9e13d31bc59f7946145ef7b61ebb8f6a01b7
Mikael Karon <mikael@karon.se>
2012-04-16 11:42:14 +0800

    refactor router to keep callbacks private.
    added test to make sure the route really changed
    
    closes #5

--- src/route/router.js
+++ src/route/router.js
@@ -3,26 +3,37 @@
  * @license TroopJS 0.0.1 Copyright 2012, Mikael Karon <mikael@karon.se>
  * Released under the MIT license.
  */
-define([ "../component/widget", "../util/uri", "callbacks" ], function RouteRouterModule(Widget, URI, Callbacks) {
-	var CALLBACKS = "callbacks";
+define([ "compose", "../component/widget", "../util/uri", "callbacks" ], function RouteRouterModule(Compose, Widget, URI, Callbacks) {
+	var NULL = null;
 
 	return Widget.extend(function RouteRouterWidget($element, name) {
-		this[CALLBACKS] = Callbacks("memory");
+		var self = this;
+		var callbacks = Callbacks("memory");
+		var oldUri = NULL;
+		var newUri = NULL;
+
+		Compose.call(self, {
+			"hub/route" : function fireRoute(topic, uri) {
+				newUri = uri.toString();
+
+				if (newUri !== oldUri) {
+					oldUri = newUri;
+
+					callbacks.fire(uri);
+				}
+			},
+
+			"hub/route/add" : function addRoute(topic, callback) {
+				callbacks.add(callback);
+			},
+
+			"hub/route/remove" : function removeRoute(topic, callback) {
+				callbacks.remove(callback);
+			}
+		});
 	}, {
 		"dom/hashchange" : function onHashChange(topic, $event) {
 			this.publish("route", URI($event.target.location.hash.replace(/^#/, "")));
-		},
-
-		"hub/route" : function fireRoute(topic, uri) {
-			this[CALLBACKS].fire(uri);
-		},
-
-		"hub/route/add" : function addRoute(topic, callback) {
-			this[CALLBACKS].add(callback);
-		},
-
-		"hub/route/remove" : function removeRoute(topic, callback) {
-			this[CALLBACKS].remove(callback);
 		}
 	});
 });
