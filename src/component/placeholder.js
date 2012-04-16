156c1fae1ed401dea53774fc3e74a05ba0e6a6fe
Mikael Karon <mikael@karon.se>
2012-04-16 13:56:18 +0800

    refactored deferred usage

--- src/component/placeholder.js
+++ src/component/placeholder.js
@@ -4,9 +4,10 @@
  * Released under the MIT license.
  */
 define([ "compose", "./widget", "jquery" ], function WidgetPlaceholderModule(Compose, Widget, $) {
+	var NULL = null;
+	var UNDEFINED = undefined;
 	var ARRAY = Array;
 	var ARRAY_PROTO = ARRAY.prototype;
-	var UNDEFINED = undefined;
 	var DATA_HOLDING = "data-holding";
 
 	return Widget.extend(function WidgetPlaceholder($element, name, _name) {
@@ -14,95 +15,108 @@ define([ "compose", "./widget", "jquery" ], function WidgetPlaceholderModule(Com
 		var _widget = UNDEFINED;
 
 		function release(/* arg, arg, arg, */ deferred) {
-			// Make arguments into a real array
-			var argv  = ARRAY.apply(ARRAY_PROTO, arguments);
+			// Internal deferred
+			$.Deferred(function deferredRelease(dfd) {
+				// Make arguments into a real array
+				var argv  = ARRAY.apply(ARRAY_PROTO, arguments);
 
-			// Update deferred to the last argument
-			deferred = argv.pop();
+				// Update deferred to the last argument
+				deferred = argv.pop();
 
-			// Add internal deferred
-			var dfd = $.Deferred();
+				// We're already holding something, reject
+				if (_widget !== UNDEFINED) {
+					dfd.reject(_widget);
+				}
+				else try {
+					// Set _widget to NULL to indicate that we're in progress
+					_widget = NULL;
 
-			// Link deferred
-			if (deferred) {
-				dfd.when(deferred.resolve, deferred.reject);
-			}
+					// Initialize deferred
+					dfd
+						.done(function done(widget) {
+							// Update _widget
+							_widget = widget;
 
-			// We're already holding something, reject
-			if (_widget !== UNDEFINED) {
-				dfd.reject(_widget);
-			}
-			else try {
-				// Initialize deferred
-				dfd.done(function done(widget) {
-					// Update _widget
-					_widget = widget;
+							// Set DATA_HOLDING attribute
+							$element.attr(DATA_HOLDING, widget);
+						})
+						.fail(function fail(widget) {
+							// Update _widget
+							_widget = widget;
+						});
 
-					// Set DATA_HOLDING attribute
-					$element.attr(DATA_HOLDING, widget);
-				});
+					// Require widget by _name
+					require([ _name ], function required(widget) {
+						// If no additional arguments, do simple instantiation
+						if (argv.length === 0) {
+							widget = widget($element, _name);
+						}
+						// Otherwise, do a complicated one
+						else {
+							// Add $element and _name to the beginning of argv
+							argv.unshift($element, _name);
 
-				// Require widget by _name
-				require([ _name ], function required(widget) {
-					// If no additional arguments, do simple instantiation
-					if (argv.length === 0) {
-						widget = widget($element, _name);
-					}
-					// Otherwise, do a complicated one
-					else {
-						// Add $element and _name to the beginning of argv
-						argv.unshift($element, _name);
+							// Instantiate
+							widget = widget.apply(widget, argv);
+						}
 
-						// Instantiate
-						widget = widget.apply(widget, argv);
-					}
+						// Build
+						widget.build();
 
-					// Build
-					widget.build();
+						// Resolve
+						dfd.resolve(widget);
+					});
+				}
+				catch (e) {
+					dfd.reject(UNDEFINED);
+				}
 
-					// Resolve
-					dfd.resolve(widget);
-				});
-			}
-			catch (e) {
-				dfd.reject(UNDEFINED);
-			}
+				// Link deferred
+				if (deferred) {
+					dfd.then(deferred.resolve, deferred.reject);
+				}
+			});
 
 			return this;
 		}
 
 		function hold(deferred) {
-			// Add internal deferred
-			var dfd = $.Deferred();
+			// Internal deferred
+			$.Deferred(function deferredHold(dfd) {
+				// First check that we're holding
+				if (_widget === UNDEFINED || _widget === NULL) {
+					dfd.reject(_widget);
+				}
+				else try {
+					// Initialize deferred
+					dfd
+						.done(function done(widget) {
+							// Remove DATA_HOLDING attribute
+							$element.removeAttr(DATA_HOLDING);
 
-			// Link deferred
-			if (deferred) {
-				dfd.when(deferred.resolve, deferred.reject);
-			}
+							// Update _widget
+							_widget = UNDEFINED;
+						})
+						.fail(function fail(widget) {
+							// Update _widget
+							_widget = UNDEFINED;
+						});
 
-			// First check that we're holding
-			if (_widget === UNDEFINED) {
-				dfd.reject(UNDEFINED);
-			}
-			else try {
-				// Initialize deferred
-				dfd.done(function done(widget) {
-					// Remove DATA_HOLDING attribute
-					$element.removeAttr(DATA_HOLDING);
+					// Destroy
+					_widget.destroy();
 
-					// Reset _widget
-					_widget = UNDEFINED;
-				});
+					// Resolve
+					dfd.resolve(_widget);
+				}
+				catch (e) {
+					dfd.reject(_widget);
+				}
 
-				// Destroy
-				_widget.destroy();
-
-				// Resolve
-				dfd.resolve(_widget);
-			}
-			catch (e) {
-				dfd.reject(UNDEFINED);
-			}
+				// Link deferred
+				if (deferred) {
+					dfd.then(deferred.resolve, deferred.reject);
+				}
+			});
 
 			return this;
 		}
