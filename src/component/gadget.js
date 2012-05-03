b26246fa8d89791dc4f697ea0a7531529404ca26
Mikael Karon <mikael@karon.se>
2012-05-03 12:02:31 +0800

    refactored lifecycle management to not depend on Compose speciffic extensions

--- src/component/gadget.js
+++ src/component/gadget.js
@@ -6,50 +6,107 @@
 /**
  * The gadget trait provides life cycle management
  */
-define([ "compose", "troopjs-compose/topdown", "deferred", "./base" ], function GadgetModule(Compose, topdown, Deferred, Component) {
+define([ "compose", "deferred", "./base" ], function GadgetModule(Compose, Deferred, Component) {
 	var STATE = "state";
-
-	function noState(state, deferred) {
-		if (deferred) {
-			deferred.resolve();
-		}
-	}
+	var INITIALIZE = "initialize";
+	var FINALIZE = "finalize";
 
 	return Component.extend(function Gadget() {
 		var self = this;
 		var __proto__ = self;
 		var callback;
-		var callbacks = [];
+		var sCallbacks = [];
+		var iCallbacks = [];
+		var fCallbacks = [];
+		var sCount = 0;
+		var iCount = 0;
+		var fCount = 0;
 		var i;
-		var iMax = 0;
 
-		add: do {
-			if (STATE in __proto__) {
+		do {
+			add: if (__proto__.hasOwnProperty(INITIALIZE)) {
+				callback = __proto__[INITIALIZE];
+
+				i = iCount;
+
+				while (i--) {
+					if (callback === iCallbacks[i]) {
+						break add;
+					}
+				}
+
+				iCallbacks[iCount++] = callback;
+			}
+
+			add: if (__proto__.hasOwnProperty(FINALIZE)) {
+				callback = __proto__[FINALIZE];
+
+				i = fCount;
+
+				while (i--) {
+					if (callback === fCallbacks[i]) {
+						break add;
+					}
+				}
+
+				fCallbacks[fCount++] = callback;
+			}
+
+			add: if (__proto__.hasOwnProperty(STATE)) {
 				callback = __proto__[STATE];
 
-				i = iMax;
+				i = sCount;
 
-				while (i--)
-					if (callback === callbacks[i]) {
-						continue add;
+				while (i--) {
+					if (callback === sCallbacks[i]) {
+						break add;
 					}
+				}
 
-				callbacks[iMax++] = callback;
+				sCallbacks[sCount++] = callback;
 			}
 		} while (__proto__ = __proto__.__proto__);
 
 		Compose.call(self, {
-			initialize : topdown(self.initialize),
-			finalize : topdown(self.finalize),
-			state : iMax !== 0
-				? function state(state, deferred) {
+			initialize : iCount <= 1
+				? self[INITIALIZE]
+				: function initialize() {
 					var _self = this;
-					var count = iMax;
+					var count = -1;
+					var length = iCount;
+
+					while (++count < length) {
+						iCallbacks[count].apply(_self, arguments);
+					}
+
+					return _self;
+				},
+
+			finalize : fCount <= 1
+				? self[FINALIZE]
+				: function finalize() {
+					var _self = this;
+					var count = -1;
+					var length = fCount;
+
+					while (++count < length) {
+						fCallbacks[count].apply(_self, arguments);
+					}
+
+					return _self;
+				},
+
+			state : sCount <= 1
+				? self[STATE]
+				: function state(state, deferred) {
+					var _self = this;
+					var count = sCount;
+					var callbacks = [];
 
 					while (--count) {
 						callbacks[count] = Deferred(function (dfd) {
-							var callback = callbacks[count];
-							var _deferred = callbacks[count + 1] || deferred;
+							var callback = sCallbacks[count];
+							var _deferred = sCallbacks[count + 1] || deferred;
 	
 							dfd.done(function done() {
 								callback.call(_self, state, _deferred);
@@ -57,12 +114,18 @@ define([ "compose", "troopjs-compose/topdown", "deferred", "./base" ], function 
 						});
 					}
 
-					callbacks[0].call(_self, state, callbacks[1] || deferred);
+					sCallbacks[0].call(_self, state, callbacks[1] || deferred);
+
+					return _self;
 				}
-				: noState
 		});
 	}, {
 		displayName : "component/gadget",
 
+		state : function state(state, deferred) {
+			if (deferred) {
+				deferred.resolve();
+			}
+		}
 	});
 });
