dee043652f7e2103a429089d29abd62904d39b5e
Mikael Karon <mikael@karon.se>
2012-05-14 16:09:53 +0800

    refactor stores

--- src/store/base.js
+++ src/store/base.js
@@ -4,10 +4,49 @@
  * Released under the MIT license.
  */
 define([ "compose", "../component/gadget" ], function StoreModule(Compose, Gadget) {
+	var STORAGE = "storage";
+
 	return Gadget.extend({
-		set : Compose.required,
-		get : Compose.required,
-		remove : Compose.required,
-		clear : Compose.required
+		storage : Compose.required,
+
+		set : function set(key, value, deferred) {
+			// JSON encoded 'value' then store as 'key'
+			this[STORAGE].setItem(key, JSON.stringify(value));
+
+			// Resolve deferred
+			if (deferred) {
+				deferred.resolve(value);
+			}
+		},
+
+		get : function get(key, deferred) {
+			// Get value from 'key', parse JSON
+			var value = JSON.parse(this[STORAGE].getItem(key));
+
+			// Resolve deferred
+			if (deferred) {
+				deferred.resolve(value);
+			}
+		},
+
+		remove : function remove(key, deferred) {
+			// Remove key
+			this[STORAGE].removeItem(key);
+
+			// Resolve deferred
+			if (deferred) {
+				deferred.resolve();
+			}
+		},
+
+		clear : function clear(deferred) {
+			// Clear
+			this[STORAGE].clear();
+
+			// Resolve deferred
+			if (deferred) {
+				deferred.resolve();
+			}
+		}
 	});
 });
--- src/store/local.js
+++ src/store/local.js
@@ -5,50 +5,9 @@
  */
 define([ "compose", "./base" ], function StoreLocalModule(Compose, Store) {
 
-	// Grab local storage
-	var STORAGE = window.localStorage;
-
 	return Compose.create(Store, {
 		displayName : "core/store/local",
 
-		set : function set(key, value, deferred) {
-			// JSON encoded 'value' then store as 'key'
-			STORAGE.setItem(key, JSON.stringify(value));
-
-			// Resolve deferred
-			if (deferred && deferred.resolve instanceof Function) {
-				deferred.resolve(value);
-			}
-		},
-
-		get : function get(key, deferred) {
-			// Get value from 'key', parse JSON
-			var value = JSON.parse(STORAGE.getItem(key));
-
-			// Resolve deferred
-			if (deferred && deferred.resolve instanceof Function) {
-				deferred.resolve(value);
-			}
-		},
-
-		remove : function remove(key, deferred) {
-			// Remove key
-			STORAGE.removeItem(key);
-
-			// Resolve deferred
-			if (deferred && deferred.resolve instanceof Function) {
-				deferred.resolve();
-			}
-		},
-
-		clear : function clear(deferred) {
-			// Clear
-			STORAGE.clear();
-
-			// Resolve deferred
-			if (deferred && deferred.resolve instanceof Function) {
-				deferred.resolve();
-			}
-		}
+		storage : window.localStorage
 	});
 });
