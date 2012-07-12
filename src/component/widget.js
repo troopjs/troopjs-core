9885570f715002882ff5de08cce020e7b6b2f4f7
Mikael Karon <mikael@karon.se>
2012-07-12 15:56:25 +0800

    fix before/afterRender

--- src/component/widget.js
+++ src/component/widget.js
@@ -79,22 +79,23 @@ define([ "./gadget", "jquery", "../util/deferred" ], function WidgetModule(Gadge
 			// Defer render (as weaving it may need to load async)
 			Deferred(function deferredRender(dfdRender) {
 
-				// After render is complete, trigger REFRESH with woven components. Add this here to make sure it's the first done hadler
-				dfdRender.done(function renderDone() {
-					$element.trigger(REFRESH, arguments);
-				});
-
 				// Link deferred
-				dfdRender.then(deferred.resolve, deferred.reject, deferred.notify);
+				dfdRender.then(function renderDone() {
+					// Trigger refresh
+					$element.trigger(REFRESH, arguments);
+
+					// Resolve outer deferred
+					deferred.resolve();
+				}, deferred.reject, deferred.notify);
 
 				// Notify that we're about to render
-				dfdRender.notify([ "beforeRender" ]);
+				dfdRender.notify("beforeRender", self);
 
 				// Call render with contents (or result of contents if it's a function)
 				$fn.call($element, contents instanceof FUNCTION ? contents.apply(self, arg) : contents);
 
 				// Notify that we're rendered
-				dfdRender.notify([ "afterRender" ]);
+				dfdRender.notify("afterRender", self);
 
 				// Weave element
 				$element.find(ATTR_WEAVE).weave(dfdRender);
