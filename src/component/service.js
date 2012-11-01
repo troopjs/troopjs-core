43e971d85ee81d318788c14d1bd638fb6efb116c
Mikael Karon <mikael@karon.se>
2012-11-01 15:15:49 +0800

    add jshint statements

--- src/component/service.js
+++ src/component/service.js
@@ -3,6 +3,8 @@
  * @license TroopJS Copyright 2012, Mikael Karon <mikael@karon.se>
  * Released under the MIT license.
  */
+/*jshint strict:false */
+/*global define:true */
 define([ "./gadget" ], function ServiceModule(Gadget) {
 	return Gadget.extend({
 		displayName : "core/component/service"
--- src/remote/ajax.js
+++ src/remote/ajax.js
@@ -3,6 +3,8 @@
  * @license TroopJS Copyright 2012, Mikael Karon <mikael@karon.se>
  * Released under the MIT license.
  */
+/*jshint strict:false */
+/*global define:true */
 define([ "../component/service", "../pubsub/topic", "jquery", "troopjs-utils/merge" ], function AjaxModule(Service, Topic, $, merge) {
 	return Service.extend({
 		displayName : "core/remote/ajax",
