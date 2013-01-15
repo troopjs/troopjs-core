c70488e1513e4c4d327f60ce8d8baf863d884133
Mikael Karon <mikael@karon.se>
2013-01-15 14:48:19 +0800

    licence update

--- src/component/base.js
+++ src/component/base.js
@@ -1,7 +1,6 @@
-/*!
+/**
  * TroopJS base component
- * @license TroopJS Copyright 2012, Mikael Karon <mikael@karon.se>
- * Released under the MIT license.
+ * @license MIT http://troopjs.mit-license.org/ © Mikael Karon mailto:mikael@karon.se
  */
 /*global define:false */
 define([ "../event/emitter" ], function ComponentModule(Emitter) {
--- src/component/gadget.js
+++ src/component/gadget.js
@@ -1,7 +1,6 @@
-/*!
+/**
  * TroopJS gadget component
- * @license TroopJS Copyright 2012, Mikael Karon <mikael@karon.se>
- * Released under the MIT license.
+ * @license MIT http://troopjs.mit-license.org/ © Mikael Karon mailto:mikael@karon.se
  */
 /*global define:false */
 define([ "./base", "when", "../pubsub/hub" ], function GadgetModule(Component, when, hub) {
