47b93692e57c4dc67b76a79f470e16094cf4ee3e
Mikael Karon <mikael@karon.se>
2013-01-18 11:06:04 +0800

    move property discovery to component base
    move signaling to component base
    
    closes #55

--- src/component/base.js
+++ src/component/base.js
@@ -3,15 +3,161 @@
  * @license MIT http://troopjs.mit-license.org/ © Mikael Karon mailto:mikael@karon.se
  */
 /*global define:false */
-define([ "../event/emitter" ], function ComponentModule(Emitter) {
-	var COUNT = 0;
-	var INSTANCE_COUNT = "instanceCount";
+define([ "../event/emitter", "when" ], function ComponentModule(Emitter, when) {
+	/*jshint laxbreak:true */
 
-	var Component = Emitter.extend(function Component() {
-		this[INSTANCE_COUNT] = COUNT++;
+	var NULL = null;
+	var ARRAY_PROTO = Array.prototype;
+	var ARRAY_UNSHIFT = ARRAY_PROTO.unshift;
+	var ARRAY_SLICE = ARRAY_PROTO.slice;
+	var INSTANCE_COUNT = "instanceCount";
+	var LENGTH = "length";
+	var FEATURES = "features";
+	var CONTEXT = "context";
+	var VALUE = "value";
+	var PROPERTIES = "properties";
+	var SIG = "sig";
+	var RE_PRO = /^(\w+)(?::(\w+))?\/(.+)/;
+	var COUNT = 0;
+
+	var Component = Emitter.extend(
+		/**
+		 * Creates a new component
+		 * @constructor
+		 */
+	function Component() {
+		var self = this;
+		var bases = self.constructor._getBases(true);
+		var base;
+		var i = bases[LENGTH];
+
+		var properties = self[PROPERTIES] = {};
+		var property;
+		var matches;
+		var key;
+		var type;
+		var name;
+
+		// Iterate base chain (backwards)
+		while((base = bases[--i])) {
+			// Iterate keys
+			for (key in base) {
+				// Continue if this is not a property on base
+				if (!base.hasOwnProperty(key)) {
+					continue;
+				}
+
+				// Continue if we can't match
+				if ((matches = RE_PRO.exec(key)) === NULL) {
+					continue;
+				}
+
+				// Get type
+				type = matches[1];
+
+				// Get or create type from properties
+				type = type in properties
+					? properties[type]
+					: properties[type] = {};
+
+				// Get name
+				name = matches[3];
+
+				// Get or create name from type
+				name = name in type
+					? type[name]
+					: type[name] = [];
+
+				// Create and set property by type/name
+				property = name[name[LENGTH]] = {};
+
+				// Init property
+				property[FEATURES] = matches[2];
+				property[CONTEXT] = base;
+				property[VALUE] = base[key];
+			}
+		}
+
+		// Update instance count
+		self[INSTANCE_COUNT] = COUNT++;
 	}, {
 		"instanceCount" : COUNT,
-		"displayName" : "core/component"
+
+		"displayName" : "core/component",
+
+		/**
+		 * Signals the component
+		 * @param signal {String} Signal
+		 * @return {*}
+		 */
+		"signal" : function onSignal(signal) {
+			var self = this;
+			var args = ARRAY_SLICE.call(arguments);
+			var signals = self[PROPERTIES][SIG][signal];
+			var length = signals
+				? signals[LENGTH]
+				: 0;
+			var index = 0;
+
+			function next(_args) {
+				// Update args
+				args = _args || args;
+
+				// Return a chained promise of next callback, or a promise resolved with args
+				return length > index
+					? when(signals[index++][VALUE].apply(self, args), next)
+					: when.resolve(args);
+			}
+
+			try {
+				// Return promise
+				return next();
+			}
+			catch (e) {
+				// Return rejected promise
+				return when.reject(e);
+			}
+		},
+
+		/**
+		 * Start the component
+		 * @return {*}
+		 */
+		"start" : function start() {
+			var self = this;
+			var _signal = self.signal;
+			var args = arguments;
+
+			// Add signal to arguments
+			ARRAY_UNSHIFT.call(args, "initialize");
+
+			return _signal.apply(self, args).then(function _start() {
+				// Modify args to change signal
+				args[0] = "start";
+
+				return _signal.apply(self, args);
+			});
+		},
+
+		/**
+		 * Stops the component
+		 * @return {*}
+		 */
+		"stop" : function stop() {
+			var self = this;
+			var _signal = self.signal;
+			var args = arguments;
+
+			// Add signal to arguments
+			ARRAY_UNSHIFT.call(args, "stop");
+
+			return _signal.apply(self, args).then(function _stop() {
+				// Modify args to change signal
+				args[0] = "finalize";
+
+				return _signal.apply(self, args);
+			});
+		}
 	});
 
 	/**
--- src/component/gadget.js
+++ src/component/gadget.js
@@ -6,79 +6,18 @@
 define([ "./base", "when", "../pubsub/hub" ], function GadgetModule(Component, when, hub) {
 	/*jshint laxbreak:true */
 
-	var UNDEFINED;
-	var NULL = null;
-	var FUNCTION = Function;
-	var ARRAY_PROTO = Array.prototype;
-	var ARRAY_SLICE = ARRAY_PROTO.slice;
-	var ARRAY_SPLICE = ARRAY_PROTO.splice;
-	var ARRAY_UNSHIFT = ARRAY_PROTO.unshift;
-	var RE_HUB = /^hub(?::(\w+))?\/(.+)/;
-	var RE_PRO = /^(\w+)(?::(\w+))?\/(.+)/;
+	var ARRAY_SPLICE = Array.prototype.splice;
 	var PUBLISH = hub.publish;
 	var REPUBLISH = hub.republish;
 	var SUBSCRIBE = hub.subscribe;
 	var UNSUBSCRIBE = hub.unsubscribe;
+	var HUB = "hub";
 	var LENGTH = "length";
 	var FEATURES = "features";
-	var CONTEXT = "context";
 	var VALUE = "value";
-	var SUBSCRIPTIONS = "subscriptions";
 	var PROPERTIES = "properties";
 
-	return Component.extend(function Gadget() {
-		var self = this;
-		var bases = self.constructor._getBases(true);
-		var base;
-		var i = bases[LENGTH];
-
-		var properties = self[PROPERTIES] = {};
-		var property;
-		var matches;
-		var key;
-		var type;
-		var name;
-
-		// Iterate base chain (backwards)
-		while((base = bases[--i])) {
-			// Iterate keys
-			for (key in base) {
-				// Continue if this is not a property on base
-				if (!base.hasOwnProperty(key)) {
-					continue;
-				}
-
-				// Continue if we can't match
-				if ((matches = RE_PRO.exec(key)) === NULL) {
-					continue;
-				}
-
-				// Get type
-				type = matches[1];
-
-				// Get or create type from properties
-				type = type in properties
-					? properties[type]
-					: properties[type] = {};
-
-				// Get name
-				name = matches[3];
-
-				// Get or create name from type
-				name = name in type
-					? type[name]
-					: type[name] = [];
-
-				// Create and set property by type/name
-				property = name[name[LENGTH]] = {};
-
-				// Init property
-				property[FEATURES] = matches[2];
-				property[CONTEXT] = base;
-				property[VALUE] = base[key];
-			}
-		}
-	}, {
+	return Component.extend({
 		"displayName" : "core/component/gadget",
 
 		/**
@@ -158,37 +97,22 @@ define([ "./base", "when", "../pubsub/hub" ], function GadgetModule(Component, w
 			}
 		},
 
-		/**
-		 * Signals the component
-		 * @param signal {String} Signal
-		 * @return {*}
-		 */
-		"signal" : function onSignal(signal) {
-			var self = this;
-			var args = ARRAY_SLICE.call(arguments);
-			var signals = self[PROPERTIES]["sig"][signal];
-			var length = signals
-				? signals[LENGTH]
-				: 0;
-			var index = 0;
+			for (key in properties) {
+				// Get subscriptions
+				subscriptions = properties[key];
 
-			function next(_args) {
-				// Update args
-				args = _args || args;
+				// Create args
+				args = [key, self];
 
-				// Return a chained promise of next callback, or a promise resolved with args
-				return length > index
-					? when(signals[index++][VALUE].apply(self, args), next)
-					: when.resolve(args);
-			}
+				// Extract callbacks into args
+				for (i = 0, iMax = subscriptions[LENGTH], j = args[LENGTH]; i < iMax; i++) {
+					args[j++] = subscriptions[i][VALUE];
+				}
 
-			try {
-				// Return promise
-				return next();
-			}
-			catch (e) {
-				// Return rejected promise
-				return when.reject(e);
+				// Did we capture any args?
+				if (j > 2) {
+					UNSUBSCRIBE.apply(hub, args);
+				}
 			}
 		},
 
@@ -229,46 +153,6 @@ define([ "./base", "when", "../pubsub/hub" ], function GadgetModule(Component, w
 			UNSUBSCRIBE.apply(hub, args);
 
 			return self;
-		},
-
-		/**
-		 * Start the component
-		 * @return {*}
-		 */
-		"start" : function start() {
-			var self = this;
-			var _signal = self.signal;
-			var args = arguments;
-
-			// Add signal to arguments
-			ARRAY_UNSHIFT.call(args, "initialize");
-
-			return _signal.apply(self, args).then(function () {
-				// Modify args to change signal
-				args[0] = "start";
-
-				return _signal.apply(self, args);
-			});
-		},
-
-		/**
-		 * Stops the component
-		 * @return {*}
-		 */
-		"stop" : function stop() {
-			var self = this;
-			var _signal = self.signal;
-			var args = arguments;
-
-			// Add signal to arguments
-			ARRAY_UNSHIFT.call(args, "stop");
-
-			return _signal.apply(self, args).then(function () {
-				// Modify args to change signal
-				args[0] = "finalize";
-
-				return _signal.apply(self, args);
-			});
 		}
 	});
 });
