e5d73bd4e51ebcb86276be0ceb311309da950aaa
Mikael Karon <mikael@karon.se>
2013-02-15 16:36:16 +0800

    Add .configure to component/service Add/remove on sig/initialize and sig/finalize (from service registry)

--- src/component/service.js
+++ src/component/service.js
@@ -4,7 +4,29 @@
  */
 /*global define:false */
 define([ "./gadget" ], function ServiceModule(Gadget) {
+	var CONFIGURATION = "configuration";
+
 	return Gadget.extend({
-		"displayName" : "core/component/service"
+		"displayName" : "core/component/service",
+
+		"sig/initialize" : function onStart() {
+			var self = this;
+
+			return self.publish("registry/add", self);
+		},
+
+		"sig/finalize" : function onFinalize() {
+			var self = this;
+
+			return self.publish("registry/remove", self);
+		},
+
+		"configure" : function configure(configuration) {
+			var self = this;
+
+			self[CONFIGURATION] = configuration || {};
+
+			return self;
+		}
 	});
 });
