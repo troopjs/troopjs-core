23aa33fd6ab6055ce463da07d1d6c4b76c9dffbf
Mikael Karon <mikael@karon.se>
2013-03-11 18:01:27 +0800

    Don't delete $element during sig/finalize

--- src/component/widget.js
+++ src/component/widget.js
@@ -175,8 +175,6 @@ define([ "./gadget", "jquery", "troopjs-utils/deferred" ], function WidgetModule
 				$element.unbind($proxy[0], $proxy[1]);
 			}
 
-			delete self[$ELEMENT];
-
 			if (deferred) {
 				deferred.resolve();
 			}
