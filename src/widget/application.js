772a2010362abb466e26011bc21112141e7f5126
Vincent.Shao <Vincent.Shao@ef.com>
2013-07-02 17:25:57 +0800

    Added support for forwarding services. See troopjs/troopjs-core#106

--- src/widget/application.js
+++ src/widget/application.js
@@ -3,16 +3,63 @@
  * @license TroopJS Copyright 2012, Mikael Karon <mikael@karon.se>
  * Released under the MIT license.
  */
-define([ "../component/widget", "troopjs-utils/deferred" ], function ApplicationModule(Widget, Deferred) {
-	return Widget.extend({
+define([ "../component/widget", "troopjs-utils/deferred", "troopjs-utils/when" ], function ApplicationModule(Widget, Deferred, When) {
+	var ARRAY_SLICE = Array.prototype.slice;
+	var SERVICES = "services";
+
+	/**
+	 * Forwards signals to services
+	 * @param signal Signal
+	 * @param deferred Deferred
+	 * @returns me
+	 */
+	function forward(signal, deferred) {
+		var me = this;
+
+		var services = tr.call(me[SERVICES], function (service) {
+			return Deferred(function (dfd) {
+				service.signal(signal, dfd);
+			});
+		});
+
+		if (deferred) {
+			When.apply($, services).then(deferred.resolve, deferred.reject, deferred.notify);
+		}
+
+		me.publish("application/signal/" + signal, deferred);
+
+		return me;
+	}
+
+	return Widget.extend(function ApplicationWidget($element, name) {
+		this[SERVICES] = ARRAY_SLICE.call(arguments, 2);
+	}, {
 		displayName : "core/widget/application",
 
+		"sig/initialize" : forward,
+
 		"sig/start" : function start(signal, deferred) {
-			this.weave(deferred);
+			var me = this;
+
+			Deferred(function (dfd) {
+				forward.call(me, signal, dfd);
+			})
+				.then(function () {
+					me.weave(deferred);
+				}, deferred.reject, deferred.progress);
 		},
 
 		"sig/stop" : function stop(signal, deferred) {
-			this.unweave(deferred);
-		}
+			var me = this;
+
+			Deferred(function (dfd) {
+				forward.call(me, signal, dfd);
+			})
+				.then(function () {
+					me.unweave(deferred);
+				}, deferred.reject, deferred.progress);
+		},
+
+		"sig/finalize" : forward
 	});
 });
