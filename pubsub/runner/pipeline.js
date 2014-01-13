8c66f1ee4129b0a9c28f25b660d5544a8087c9a3
Mikael Karon <mikael@karon.se>
2014-01-13 17:28:10 +0800

    Add new runners for pubsub/hub

---
+++ pubsub/runner/pipeline.js
@@ -0,0 +1,55 @@
+define([ "when" ], function (when) {
+	var UNDEFINED;
+	var PHASE = "phase";
+	var RE_PHASE = /^(?:initi|fin)alized?$/;
+	var MEMORY = "memory";
+	var CONTEXT = "context";
+	var CALLBACK = "callback";
+	var HANDLED = "handled";
+
+	/*
+	 * Constructs a function that executes handlers in a pipeline without overlap
+	 * @private
+	 * @param {Array} handlers Array of handlers
+	 * @param {Number} handled Handled counter
+	 * @param {Object} [anchor={}] Object for saving MEMORY on
+	 * @returns {Function}
+	 */
+	return function (handlers, handled, anchor) {
+		// Default value for anchor
+		anchor = anchor || {};
+
+		var handlersCount = 0;
+		var result;
+
+		/*
+		 * Internal function for piped execution of handlers handlers
+		 * @private
+		 * @param {Array} [args] result from previous handler callback
+		 * @return {Promise} promise of next handler callback execution
+		 */
+		var next = function (args) {
+			/*jshint curly:false*/
+			var context;
+			var handler;
+
+			// Check that we have args
+			if (args !== UNDEFINED) {
+				// Update memory and result
+				anchor[MEMORY] = result = args;
+			}
+
+			// Iterate until we find a handler in a blocked phase
+			while ((handler = handlers[handlersCount++])	// Has next handler
+				&& (context = handler[CONTEXT])				// Has context
+				&& RE_PHASE.test(context[PHASE]));			// In blocked phase
+
+			// Return promise of next callback,or promise resolved with result
+			return handler
+				? (handler[HANDLED] = handled) === handled && when(handler[CALLBACK].apply(context, result), next)
+				: when.resolve(result);
+		};
+
+		return next;
+	}
+});
---
+++ pubsub/runner/sequence.js
@@ -0,0 +1,54 @@
+define([ "when" ], function (when) {
+	var PHASE = "phase";
+	var RE_PHASE = /^(?:initi|fin)alized?$/;
+	var CONTEXT = "context";
+	var CALLBACK = "callback";
+	var LENGTH = "length";
+	var HANDLED = "handled";
+
+	/*
+	 * Constructs a function that executes handlers in sequence without overlap
+	 * @private
+	 * @param {Array} handlers Array of handlers
+	 * @param {Number} handled Handled counter
+	 * @param {Array} [result=[]] Result array
+	 * @returns {Function}
+	 */
+	return function (handlers, handled, result) {
+		// Default value for result
+		result = result || [];
+
+		var handlersCount = 0;
+		var resultLength = result[LENGTH];
+		var resultCount = resultLength - 1;
+
+		/*
+		 * Internal function for sequential execution of handlers handlers
+		 * @private
+		 * @param {Array} [args] result from previous handler callback
+		 * @return {Promise} promise of next handler callback execution
+		 */
+		var next = function (args) {
+			/*jshint curly:false*/
+			var context;
+			var handler;
+
+			// Store result
+			if (resultCount++ >= resultLength) {
+				result[resultCount] = args;
+			}
+
+			// Iterate until we find a handler in a blocked phase
+			while ((handler = handlers[handlersCount++])	// Has next handler
+				&& (context = handler[CONTEXT])				// Has context
+				&& RE_PHASE.test(context[PHASE]));			// In blocked phase
+
+			// Return promise of next callback, or a promise resolved with result
+			return handler
+				? (handler[HANDLED] = handled) === handled && when(handler[CALLBACK].apply(context, args), next)
+				: when.resolve(result);
+		};
+
+		return next;
+	};
+});
