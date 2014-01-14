697f4c184fe475ec31597ba533c3992df0508611
Mikael Karon <mikael@karon.se>
2014-01-14 16:45:58 +0800

    Update API and implementation for runners.

--- event/runner/sequence.js
+++ event/runner/sequence.js
@@ -1,45 +1,38 @@
 define([ "when" ], function (when) {
+	var UNDEFINED;
 	var CONTEXT = "context";
 	var CALLBACK = "callback";
-	var LENGTH = "length";
 	var HANDLED = "handled";
 
 	/*
 	 * Constructs a function that executes handlers in sequence without overlap
-	 * @private
 	 * @param {Array} handlers Array of handlers
 	 * @param {Number} handled Handled counter
-	 * @param {Array} [result=[]] Result array
+	 * @param {Array} args Initial arguments
 	 * @returns {Function}
 	 */
-	return function (handlers, handled, result) {
-		// Default value for result
-		result = result || [];
-
+	return function (handlers, handled, args) {
+		var results = [];
+		var resultsCount = -1;
 		var handlersCount = 0;
-		var resultLength = result[LENGTH];
-		var resultCount = resultLength - 1;
 
 		/*
 		 * Internal function for sequential execution of handlers handlers
 		 * @private
-		 * @param {Array} [args] result from previous handler callback
+		 * @param {Array} [result] result from previous handler callback
 		 * @return {Promise} promise of next handler callback execution
 		 */
-		var next = function (args) {
+		var next = function (result) {
 			/*jshint curly:false*/
-			var context;
 			var handler;
 
 			// Store result
-			if (resultCount++ >= resultLength) {
-				result[resultCount] = args;
-			}
+			results[resultsCount++] = result;
 
 			// Return promise of next callback, or a promise resolved with result
-			return (handler = handlers[handlersCount++])
+			return (handler = handlers[handlersCount++]) !== UNDEFINED
 				? (handler[HANDLED] = handled) === handled && when(handler[CALLBACK].apply(handler[CONTEXT], args), next)
-				: when.resolve(result);
+				: when.resolve(results);
 		};
 
 		return next;
--- pubsub/runner/sequence.js
+++ pubsub/runner/sequence.js
@@ -1,52 +1,47 @@
 define([ "when" ], function (when) {
-	var PHASE = "phase";
-	var RE_PHASE = /^(?:initi|fin)alized?$/;
+	var UNDEFINED;
 	var CONTEXT = "context";
 	var CALLBACK = "callback";
-	var LENGTH = "length";
 	var HANDLED = "handled";
+	var PHASE = "phase";
+	var RE_PHASE = /^(?:initi|fin)alized?$/;
 
 	/*
 	 * Constructs a function that executes handlers in sequence without overlap
-	 * @private
 	 * @param {Array} handlers Array of handlers
 	 * @param {Number} handled Handled counter
-	 * @param {Array} [result=[]] Result array
+	 * @param {Array} args Initial arguments
 	 * @returns {Function}
 	 */
-	return function (handlers, handled, result) {
-		// Default value for result
-		result = result || [];
-
+	return function (handlers, handled, args) {
+		var results = [];
+		var resultsCount = -1;
 		var handlersCount = 0;
-		var resultLength = result[LENGTH];
-		var resultCount = resultLength - 1;
 
 		/*
 		 * Internal function for sequential execution of handlers handlers
 		 * @private
-		 * @param {Array} [args] result from previous handler callback
+		 * @param {Array} [result] result from previous handler callback
 		 * @return {Promise} promise of next handler callback execution
 		 */
-		var next = function (args) {
+		var next = function (result) {
 			/*jshint curly:false*/
-			var context;
 			var handler;
+			var context;
 
 			// Store result
-			if (resultCount++ >= resultLength) {
-				result[resultCount] = args;
-			}
+			results[resultsCount++] = result;
 
-			// Iterate until we find a handler in a blocked phase
-			while ((handler = handlers[handlersCount++])	// Has next handler
-				&& (context = handler[CONTEXT])				// Has context
-				&& RE_PHASE.test(context[PHASE]));			// In blocked phase
+			// TODO Needs cleaner implementation
+			// Iterate handlers while handler has a context and that context is in a blocked phase
+			while ((handler = handlers[handlersCount++]) // Has next handler
+				&& (context = handler[CONTEXT])            // Has context
+				&& RE_PHASE.test(context[PHASE]));         // In blocked phase
 
 			// Return promise of next callback, or a promise resolved with result
-			return handler
+			return handler !== UNDEFINED
 				? (handler[HANDLED] = handled) === handled && when(handler[CALLBACK].apply(context, args), next)
-				: when.resolve(result);
+				: when.resolve(results);
 		};
 
 		return next;
