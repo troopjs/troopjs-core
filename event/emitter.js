460a70f566891e2ec5b04fd0b22c2719039381b5
Mikael Karon <mikael@karon.se>
2014-03-06 18:29:42 +0800

    Move insterface to `jsduck.js`

--- event/emitter.js
+++ event/emitter.js
@@ -248,23 +248,5 @@ define([
 
 	Emitter.createHandlers = createHandlers;
 
-	/**
-	 * The runner interface
-	 * @class core.event.emitter.runner
-	 * @abstract
-	 * @singleton
-	 */
-
-	/**
-	 * Run event handlers.
-	 * @abstract
-	 * @method constructor
-	 * @param {Object} event Event object
-	 * @param {String} event.context Event context
-	 * @param {Function} event.callback Event callback
-	 * @param {Object} handlers List of handlers
-	 * @param {Array} args Initial arguments
-	 */
-
 	return Emitter;
 });
---
+++ jsduck.js
@@ -0,0 +1,16 @@
+/**
+ * The runner interface
+ * @class core.event.emitter.runner
+ * @abstract
+ */
+
+/**
+ * Run event handlers.
+ * @abstract
+ * @method constructor
+ * @param {Object} event Event object
+ * @param {String} event.context Event context
+ * @param {Function} event.callback Event callback
+ * @param {Object} handlers List of handlers
+ * @param {Array} args Initial arguments
+ */
