1a7ca6a7bc21fc7f76d37f56788340369fcd9f2c
Mikael Karon <mikael@karon.se>
2014-04-10 11:51:03 +0800

    Refactor service.

--- component/service.js
+++ component/service.js
@@ -5,7 +5,7 @@ define([ "./gadget" ], function ServiceModule(Gadget) {
 	"use strict";
 
 	/**
-	 * Base class for all service alike components, self-registering.
+	 * Base class for all service alike components.
 	 *
 	 * @class core.component.service
 	 * @extends core.component.gadget
@@ -16,28 +16,6 @@ define([ "./gadget" ], function ServiceModule(Gadget) {
 	 * @inheritdoc
 	 */
 	return Gadget.extend({
-		"displayName" : "core/component/service",
-
-		/**
-		 * @inheritdoc
-		 * @localdoc Registers service with the {@link core.registry.service service registry}
-		 * @handler
-		 */
-		"sig/initialize" : function onInitialize() {
-			var me = this;
-
-			return me.publish("registry/add", me);
-		},
-
-		/**
-		 * @inheritdoc
-		 * @localdoc Un-registers service with the {@link core.registry.service service registry}
-		 * @handler
-		 */
-		"sig/finalize" : function onFinalize() {
-			var me = this;
-
-			return me.publish("registry/remove", me);
-		}
+		"displayName" : "core/component/service"
 	});
 });
