862460947676d5d8158fe9e6fc182a91585590eb
Mikael Karon <mikael@karon.se>
2014-10-16 17:19:08 +0800

    Refactor signals

---
+++ component/signal/stop.js
@@ -0,0 +1,42 @@
+/**
+ * @license MIT http://troopjs.mit-license.org/
+ */
+define([
+	"./start",
+	"when"
+], function (start, when) {
+	var ARRAY_PUSH = Array.prototype.push;
+	var PHASE = "phase";
+
+	/**
+	 * @class core.component.signal.stop
+	 * @mixin Function
+	 * @static
+	 */
+
+	return function stop() {
+		var me = this;
+		var args = arguments;
+
+		return when(start.apply(me, args), function (phase) {
+			var _args;
+
+			if (phase === "started") {
+				// Let `me[PHASE]` be `"stop"`
+				// Let `_args` be `[ "stop" ]`
+				// Push `args` on `_args`
+				ARRAY_PUSH.apply(_args = [ me[PHASE] = "stop" ], args);
+
+				return me
+					.signal.apply(me, _args)
+					.then(function () {
+						// Let `me[PHASE]` be `"stopped"`
+						return me[PHASE] = "stopped";
+					});
+			}
+			else {
+				return phase;
+			}
+		});
+	}
+});
