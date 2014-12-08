088d7c013b63e2df5799cab48f32becdadc73f5c
Mikael Karon <mikael@karon.se>
2014-12-08 13:00:27 +0800

    Refactor registry - major simplification.

--- registry/emitter.js
+++ registry/emitter.js
@@ -5,7 +5,8 @@ define([
 	"../event/emitter",
 	"../config",
 	"../component/runner/sequence",
-	"poly/array"
+	"poly/array",
+	"poly/object"
 ], function (Emitter, config, sequence) {
 	"use strict";
 
@@ -15,18 +16,12 @@ define([
 	 * @extend core.event.emitter
 	 */
 
-	var UNDEFINED;
 	var TYPE = "type";
 	var RUNNER = "runner";
 	var LENGTH = "length";
 	var INDEX = "index";
-	var KEY = "key";
-	var VALUE = "value";
-	var INDEX_KEY = "index_key";
-	var INDEX_POS = "index_pos";
 	var OBJECT_TOSTRING = Object.prototype.toString;
-	var TOSTRING_STRING = "[object String]";
-	var REGEXP_STRING = "[object RegExp]";
+	var TOSTRING_REGEXP = "[object RegExp]";
 	var SIG_REGISTER = config.signal.register;
 	var SIG_UNREGISTER = config.signal.unregister;
 
@@ -62,129 +57,78 @@ define([
 		 * @property {Number} index_key.index Entry index
 		 * @property {*} index_key.value Entry value
 		 */
-		this[INDEX_KEY] = {};
-
-		/**
-		 * Registry pos storage
-		 * @private
-		 * @readonly
-		 * @property {Object[]} index_pos
-		 * @property {String} index_pos.key Entry key
-		 * @property {Number} index_pos.index Entry index
-		 * @property {*} index_pos.value Entry value
-		 */
-		this[INDEX_POS] = [];
+		this[INDEX] = {};
 	}, {
 		"displayName": "core/registry/emitter",
 
-		/**
-		 * Either gets or puts values into the registry.
-		 *
-		 * - If no key is provided, all entries in the registry are returned.
-		 * - If no value is provided, it depends on the **key**
-		 *   - if key is string or number, current value under this key is returned.
-		 *   - if key is regexp, all values whose key match this pattern are returned
-		 * - If value is provided it replaces the current value for the key
-		 * @param {String|Number|RegExp} [key] Entry key, index or pattern
-		 * @param {*} [value] Entry value
-		 * @return {*} All values if no key, current value for key if no value provided, otherwise the provided value if a new entry is created
-		 * @throws Error if a new entry is created and key is not of type String
-		 */
-		"access": function (key, value) {
-			var me = this;
-			var index_key = me[INDEX_KEY];
-			var index_pos = me[INDEX_POS];
+		"get": function (key) {
+			var index = this[INDEX];
 			var result;
-			var argc;
-			var event;
 
-			// Reading _all_
-			if ((argc = arguments[LENGTH]) === 0) {
-				result = index_pos.map(function (item) {
-					return item[VALUE];
-				});
+			if (arguments[LENGTH] === 0) {
+				result = Object
+					.keys(index)
+					.map(function (_key) {
+						return index[_key];
+					});
 			}
-			// query registry by keys
-			else if (OBJECT_TOSTRING.call(key) === REGEXP_STRING && value === UNDEFINED){
-				result = Object.keys(index_key).filter(function (name) {
-						return key.test(name);
-				}).map(function map(key) {
-					return index_key[key][VALUE];
-				});
+			else if (OBJECT_TOSTRING.call(key) === TOSTRING_REGEXP) {
+				result = Object
+					.keys(index)
+					.filter(function (_key) {
+						return key.test(_key);
+					}).map(function (_key) {
+						return index[_key];
+					});
 			}
 			else {
-				result = (typeof key === 'number' ? index_pos : index_key)[key];
-
-				// Reading
-				if (argc === 1) {
-					result = result !== UNDEFINED ? result[VALUE] : UNDEFINED;
-				}
-				// Writing
-				else {
-					// Replace existing entry
-					if (result !== UNDEFINED) {
-						result = result[VALUE] = value;
-					}
-					// Check type of key (as now we're creating a new one)
-					else if (OBJECT_TOSTRING.call(key) !== TOSTRING_STRING) {
-						throw Error("key has to be of type String");
-					}
-					// Create new entry
-					else {
-						result = {};
-						result = index_key[result[KEY] = key] = index_pos[result[INDEX] = index_pos[LENGTH]] = result;
-						result = result[VALUE] = value;
-					}
-
-					// Let `event` be `{}`
-					event = {};
-					// Let `event[RUNNER]` be `sequence`
-					event[RUNNER] = sequence;
-					// Let `event[TYPE` be `SIG_REGISTER`
-					event[TYPE] = SIG_REGISTER;
-					// Emit
-					me.emit(event, key, result);
-				}
+				result = index[key];
 			}
 
 			return result;
 		},
 
-		/**
-		 * Removes entries from the registry
-		 * TODO: Fixed screwed up index when item is removed from registry.
-		 *
-		 * - If no key is provided, all entries in the registry are removed.
-		 * - Otherwise only the corresponding entry for key is removed.
-		 * @param {String|Number} [key] Entry key or index
-		 *
-		 */
-		"remove": function (key) {
+		"register": function (key, value) {
 			var me = this;
-			var result;
-			var index_key = me[INDEX_KEY];
-			var index_pos = me[INDEX_POS];
+			var index = me[INDEX];
 			var event;
 
-			// Remove all entries
-			if (arguments[LENGTH] === 0) {
-				me[INDEX_KEY] = {};
-				me[INDEX_POS] = [];
-			}
-			// Remove entry by key
-			else if ((result = index_key[key]) !== UNDEFINED) {
-				delete index_key[result[KEY]];
-				delete index_pos[result[INDEX]];
+			if (index[key] !== value) {
 
-				// Let `event` be `{}`
+				if (index.hasOwnProperty(key)) {
+					me.unregister(key);
+				}
+
 				event = {};
-				// Let `event[RUNNER]` be `sequence`
+				event[TYPE] = SIG_REGISTER;
 				event[RUNNER] = sequence;
-				// Let `event[TYPE` be `SIG_UNREGISTER`
-				event[TYPE] = SIG_UNREGISTER;
-				// Emit
-				me.emit(event, key, result[VALUE]);
+
+				me.emit(event, key, index[key] = value);
 			}
+
+			return value;
+		},
+
+		"unregister": function (key) {
+			var me = this;
+			var index = me[INDEX];
+			var value;
+			var event;
+
+			if (index.hasOwnProperty(key)) {
+
+				value = index[key];
+
+				if (delete index[key]) {
+					event = {};
+					event[TYPE] = SIG_UNREGISTER;
+					event[RUNNER] = sequence;
+
+					me.emit(event, key, value);
+				}
+			}
+
+			return value;
 		}
 	});
 });
--- test/registry/emitter-test.js
+++ test/registry/emitter-test.js
@@ -8,62 +8,45 @@ buster.testCase("troopjs-core/registry/emitter", function (run) {
 	require( [ "troopjs-core/registry/emitter" ] , function (Registry) {
 		run({
 			"when starting from scratch": {
-				"setUp": function () {
-					this.registry = Registry();
+				"returns empty array when .get() on empty registry": function () {
+					assert.match(Registry().get(), []);
 				},
 
-				"STORAGE is empty": function () {
-					assert.match(this.registry.access(), []);
-				},
-
-				"does not iterate": function () {
-					var spy = this.spy();
-
-					this.registry.access().forEach(spy);
-
-					refute.called(spy);
-				},
-
-				"returns UNDEFINED when calling .access with a key": function () {
-					refute.defined(this.registry.access("key"));
+				"returns UNDEFINED when .get(key) on empty registry": function () {
+					refute.defined(Registry().get("key"));
 				},
 
 				"can store and retrieve by key": function () {
-					var item = this.registry.access("key", {});
+					var registry = Registry();
+					var item = registry.register("key", {});
 
-					assert.same(item, this.registry.access("key"));
+					assert.same(item, registry.get("key"));
 				},
 
-				"can store by key and retrieve by index": function () {
-					var item = this.registry.access("key", {});
+				"can store and iterate": function () {
+					var registry = Registry();
+					var item = registry.register("key", {});
 
-					assert.same(item, this.registry.access(0));
+					assert.equals([ item ], registry.get());
 				},
 
-				"can store by key and update by index": function () {
-					var registry = this.registry;
-					var item = registry.access("key", {});
+				"can store, filter and iterate": function () {
+					var registry = Registry();
+					var item1 = registry.register("key1", {});
+					registry.register("2key", {});
+					var item3 = registry.register("key3", {});
 
-					assert.same(item, registry.access(0));
-
-					item = registry.access(0, {});
-
-					assert.same(item, registry.access(0));
+					assert.equals([ item1, item3 ], registry.get(/^key.+/));
 				},
 
-				"can store by key and iterate": function () {
-					var item = this.registry.access("key", {});
-					var spy = this.spy();
+				"can unregister by key": function () {
+					var registry = Registry();
 
-					this.registry.access().forEach(spy);
+					registry.register("key", {});
+					assert.defined(registry.get("key"));
 
-					assert.calledOnceWith(spy, item, 0);
-				},
-
-				"throws if entry is created without a string key": function () {
-					assert.exception(function () {
-						this.registry.access(0, {});
-					});
+					registry.unregister("key");
+					refute.defined(registry.get("key"));
 				}
 			}
 		});
