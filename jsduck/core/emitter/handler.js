487540e06d86f7dff6adae0df4dd22e34a81de43
Mikael Karon <mikael@karon.se>
2015-02-12 18:55:35 +0800

    JSDoc fixes.

---
+++ jsduck/core/emitter/handler.js
@@ -0,0 +1,24 @@
+/**
+ * The handler interface
+ * @class core.emitter.handler
+ * @mixin Function
+ * @interface
+ * @private
+ */
+
+/**
+ * @method constructor
+ * @abstract
+ * @param {core.emitter.composition} emitter
+ * @param {String} type
+ * @param {Function} callback
+ * @param {*} [data]
+ */
+
+/**
+ * Executes an emission
+ * @method handle
+ * @abstract
+ * @param {*[]} args Handler arguments
+ * @returns {*} Result from handler
+ */
